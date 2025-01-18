88020011a066b1481c087e74722ff31d84af3f43
Aniket Madhe <aniketmadhe25@gmail.com>
2025-01-18 23:06:55 +0530

    footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="w-full h-10 bg-slate-200 flex justify-center items-center">
+      <h4 className="font-semibold text-black text-[13px]">
+        © 2025 Bookworm. All Rights Reserved.
+      </h4>
+    </div>
+  );
 }
 
 export default Footer;
